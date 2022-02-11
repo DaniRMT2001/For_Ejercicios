95737312423ec896bc44294dce32a4c634b10e89
roberto daniel marte tejada <danielrmt2001@gmail.com>
2022-02-11 07:59:53 -0400

    Ejercicios for2

--- script.js
+++ script.js
@@ -48,13 +48,36 @@ function quinto() {
 
 function sexto() {
     x = document.getElementById("sexto").value;
-    x=charAT(x.length()-1)
+    x = charAT(x.length() - 1)
     x = parseInt(x)
-    for (let i=1; i <= x; i++) {
-       console.log(i)
+    for (let i = 1; i <= x; i++) {
+        console.log(i)
     }
 
-    
+
 
 }
+
+function septimo() {
+    x = [[0, 1], [1, 1], [2, 1], [3, 1], [4, 2], [5, 2], [6, 2], [7, 2]]
+    for (let i in x) {
+        console.log(x[i])
+    }
+}
+
+function octavo() {
+    x = [[0, 1], [1, 1], [2, 2], [3, 2], [4, 3], [5, 3], [6, 4], [7, 4], [8, 5], [9, 5]]
+    for(let i in x){
+        console.log(x[i])
+    }
+}
+
+function noveno(){
+    x = [[ 1, 1, 1,], [2, 1, 2], [3, 1,3], [4, 2,1], [5,2,2 ], [6, 2,3], [7,3, 1], [8,3, 2], [9, 3,3]]
+    for(let i in x){
+        console.log(x[i])
+    }
+}
+
+
 
